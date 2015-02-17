e73e74f66c6c411d7f32f12803d06fdfbbbca296
Jason Fan <sevengram1991@gmail.com>
2015-02-17 22:46:27 +0800

    1. Add user agent to http.py 2. Add some util functions in http.py

--- http.py
+++ http.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import json
+import mimetypes
 
 import tornado.gen
 import tornado.httpclient
@@ -8,6 +9,7 @@ import tornado.httputil
 
 from util import dtools
 
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'
 
 type_methods = {
     'json': json.dumps,
@@ -21,7 +23,7 @@ type_methods = {
 def _send_dict(url, method, data, data_type, headers):
     _headers = headers or {}
     if data_type == 'form':
-        _headers['Content-Type'] = 'application/x-www-form-urlencoded'
+        _headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
     client = tornado.httpclient.AsyncHTTPClient()
     req = tornado.httpclient.HTTPRequest(
         url=url,
@@ -46,11 +48,39 @@ def put_dict(url, data, data_type='form', headers=None):
 
 
 @tornado.gen.coroutine
-def get_dict(url, data):
+def get_dict(url, data, headers=None):
     client = tornado.httpclient.AsyncHTTPClient()
     req = tornado.httpclient.HTTPRequest(
         url=url + '?' + dtools.urlencode(data),
-        method='GET'
+        method='GET',
+        headers=tornado.httputil.HTTPHeaders(headers or {})
     )
     resp = yield client.fetch(req)
     raise tornado.gen.Return(resp)
+
+
+def get_content_type(filename):
+    return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+
+
+def encode_multipart_formdata(fields, files):
+    boundary = '----------ThIs_Is_tHe_bouNdaRY_$'
+    crlf = '\r\n'
+    l = []
+    for (key, value) in fields:
+        l.append('--' + boundary)
+        l.append('Content-Disposition: form-data; name="%s"' % key)
+        l.append('')
+        l.append(value)
+    for (key, filename, value) in files:
+        l.append('--' + boundary)
+        l.append('Content-Disposition: form-data; name="%s"; filename="%s"' %
+                 (key, filename.split('/')[-1]))
+        l.append('Content-Type: %s' % get_content_type(filename))
+        l.append('')
+        l.append(value)
+    l.append('--' + boundary + '--')
+    l.append('')
+    body = crlf.join(l)
+    content_type = 'multipart/form-data; boundary=%s' % boundary
+    return content_type, body
